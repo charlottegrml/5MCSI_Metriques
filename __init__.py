710fcacd8c2c11a2dd42f45b8634ab7a4434e996
charlottegrml <charlotte.gourmelond@gmail.com>
2026-01-05 17:14:34 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -35,6 +35,32 @@ def mongraphique():
 def mongraphique2():
     return render_template("graphique.html")
 
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+    date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+    minutes = date_object.minute
+    return jsonify({'minutes': minutes})
+
+@app.route('/api/commits/')
+def commits_api():
+    url = "https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits"
+    response = requests.get(url)
+    commits = response.json()
+
+    minutes_count = {}
+
+    for commit in commits:
+        date_str = commit['commit']['author']['date']
+        date_obj = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
+        minute = date_obj.minute
+
+        if minute in minutes_count:
+            minutes_count[minute] += 1
+        else:
+            minutes_count[minute] = 1
+
+    return jsonify(minutes_count)
+
 
   
 if __name__ == "__main__":
